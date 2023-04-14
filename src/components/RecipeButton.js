2cfe087b0ee313eab5e144c82f1aff91c52b040d
Avarice <113299175+Avarice03@users.noreply.github.com>
2023-04-14 20:19:42 +0800

    Update RecipeButton.js

--- src/components/RecipeButton.js
+++ src/components/RecipeButton.js
@@ -3,7 +3,8 @@ import { UserContext } from "./providers/User";
 import imagePlaceholder from "./images/photo.png";
 
 function RecipeButton({ id, image, name, handleDelete, showRecipe }) {
-
+  const [admin, setAdmin] = useContext(UserContext);
+  
   return (
     <>
       <div className="recipe-card">
