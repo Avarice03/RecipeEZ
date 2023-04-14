1c639e6bbdc692f052ebdc364b60d8c19985588e
Avarice <113299175+Avarice03@users.noreply.github.com>
2023-04-14 20:00:00 +0800

    Update Login.js

--- p3-react-app/src/components/pages/Login.js
+++ p3-react-app/src/components/pages/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import chef from "../images/chef.png";
 import "../stylesheets/Login.css";
 
